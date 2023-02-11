1b4b2bf845d337045e29c45ff2d5a68f5669a898
luigialejandro <50028217+luigialejandro@users.noreply.github.com>
2023-02-10 18:44:39 -0600

    Working with PaisInputComponent so as to recover the searchbar functionality just like before. To achieve this, I created a new onEnter event (an EventEmitter type) that emits the search bar input (termino) every time a term is submitted. PorPaisComponent (the parent) listens to this event and calls the buscar method to execute the request.

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -16,9 +16,10 @@ export class PorPaisComponent {
 
   constructor( private paisService: PaisService ) {}
 
-  buscar() {
+  buscar( termino: string ) {
     this.hayError = false;
-    console.log(this.termino);
+    this.termino = termino;
+    //console.log(this.termino);
 
     this.paisService.buscarPais( this.termino )
       .subscribe( (paises) => {
